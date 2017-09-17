fad24d763edf60241b65262318d483bb8a06cf35
asahi <hty980204@gmail.com>
2017-09-17 14:01:27 +0800

    update

---
+++ wx_t/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class WxTConfig(AppConfig):
+    name = 'wx_t'
---
+++ wx_t/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import hashlib
+# Create your views here.
+
+def handle(request):
+    print(request.body)
+    token='anhedonia'
+    try:
+        data=request.GET
+        if len(data)==0:
+            return 'No data'
+        siganture=data.siganture
+        nonce = data.nonce
+        echostr = data.echostr
+        ist = [token, timestamp, nonce]
+        list.sort()
+        sha1 = hashlib.sha1()
+        map(sha1.update, list)
+        hashcode = sha1.hexdigest()
+        print("handle/GET func: hashcode, signature: ", hashcode, signature)
+        if hashcode == signature:
+            return echostr
+        else:
+            return ""
+    except Exception,Argument:
+        return Argument
+            
